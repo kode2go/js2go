0d334299230bed4e0e00c5b99db42cf4b1a2069e
kode2go <brsbin001@myuct.ac.za>
2022-05-24 10:28:15 +0200

    Create main.js

---
+++ lesson4/main.js
@@ -0,0 +1,60 @@
+// functions
+
+// function addNums(num1, num2)
+// defaults
+function addNums(num1=1, num2=2)
+{
+	console.log(num1+num2)
+	return num1 + num2
+}
+
+addNums(5,4)
+// ouput not a number if no default
+addNums()
+
+console.log(addNums(5,4))
+
+// arrow function:
+
+const addNumsArrow = (num1=1, num2=2) =>
+{
+	console.log(num1+num2)
+	return num1 + num2
+}
+
+addNumsArrow(5,4)
+// ouput not a number if no default
+addNumsArrow()
+
+console.log(addNumsArrow(5,4))
+
+// if its only one expression we don't need curly brackets or return:
+
+// const addNumsArrowLine = (num1=1, num2=2) => console.log(num1+num2)
+
+const addNumsArrowLine = (num1=1, num2=2) => num1+num2
+
+
+addNumsArrowLine(5,4)
+// ouput not a number if no default
+addNumsArrowLine()
+
+console.log(addNumsArrowLine(5,4))
+
+// and slim it down even further if you have only one param:
+
+const addNumsArrowLineSlim = num1 => num1+5
+
+
+addNumsArrowLineSlim(5)
+// ouput not a number if no default
+addNumsArrowLineSlim()
+
+console.log(addNumsArrowLineSlim(5))
+
+
+// can use it with foreach:
+// quite handy for quite and easy function (lambda - python)
+todos.forEach((todo) => console.log(todo))
+
+// can use it with lexical this
